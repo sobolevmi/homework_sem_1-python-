0d10e3a1013c111cb6d515ed69246a28643187f9
sobolevmi <107508261+sobolevmi@users.noreply.github.com>
2022-09-03 18:34:45 +0300

    исправлена функция def

--- task2.py
+++ task2.py
@@ -1,13 +1,19 @@
 # Программа для проверки истинности утверждения not (x V y V z) = not x ⋀ not y ⋀ not z
 
-def is_statement_right(x, y, z):
-    return not (x or y or z) == (not x and not y and not z)
+def statement_check(x, y, z):
+    first_statement = not (x or y or z)
+    second_statement = not x and not y and not z
+    if first_statement == second_statement:
+        statement = True
+    else:
+        statement = False
+    return statement
 
 predicate_x = str(input("Введите значение предиката X: "))
 predicate_y = str(input("Введите значение предиката Y: "))
 predicate_z = str(input("Введите значение предиката Z: "))
 
-result = is_statement_right(predicate_x, predicate_y, predicate_z)
+result = statement_check(predicate_x, predicate_y, predicate_z)
 if result == True:
     print("Утверждение верно")
 else:
