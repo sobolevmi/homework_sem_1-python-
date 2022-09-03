1006dc66d18818297605641404968022c93c6d3e
Maksim Sobolev <sobmig@gmail.com>
2022-09-03 17:22:27 +0300

    добавлен вывод сообщения о неверном числе при вводе пользователем числа меньше 1

--- task1.py
+++ task1.py
@@ -2,7 +2,7 @@
 # и проверяет, является ли этот день выходным
 
 user_number = int(input("Введите порядковый номер дня недели (от 1 до 7): "))
-if user_number <= 7:
+if user_number >= 1 and user_number <= 7:
     if user_number <= 5:
         print(f"- {user_number} -> нет")
     else:
