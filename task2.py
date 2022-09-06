a4f38c38b3f6e6497a49333f350518f31c44ea36
Maksim Sobolev <sobmig@gmail.com>
2022-09-07 02:06:35 +0300

    исправлено решение через 3 вложенных цикла

--- task2.py
+++ task2.py
@@ -1,24 +1,9 @@
 # Программа для проверки истинности утверждения not (x V y V z) = not x ⋀ not y ⋀ not z
 
-def statement_check(x, y, z):
-    first_statement = not (x or y or z)
-    second_statement = not x and not y and not z
-    if first_statement == second_statement:
-        statement = True
-    else:
-        statement = False
-    return statement
-
-predicate_x = str(input("Введите значение предиката X: "))
-predicate_y = str(input("Введите значение предиката Y: "))
-predicate_z = str(input("Введите значение предиката Z: "))
-
-result = statement_check(predicate_x, predicate_y, predicate_z)
-if result == True:
-    print("Утверждение верно")
-else:
-    print("Утверждение ложно")
-
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            print(f"{x} {y} {z} {not (x or y or z) == (not x and not y and not z)}")
 
 
 
